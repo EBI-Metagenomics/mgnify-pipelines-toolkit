276fac9fbe27e1b525accc6710a8699e270238bf
Christian Atallah <chrisata@ebi.ac.uk>
2024-02-15 11:28:15 +0000

    make final changes for amplicon V6 before bioconda submission

--- mgnify_pipelines_toolkit/analysis/amplicon/remove_ambiguous_reads.py
+++ mgnify_pipelines_toolkit/analysis/amplicon/remove_ambiguous_reads.py
@@ -21,14 +21,14 @@ import gzip
 
 from Bio import SeqIO, bgzf
 
-def parse_args(argv=None):
+def parse_args():
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-f", "--fwd", required=True, type=str, help="Path to forward (or single-end) fastq file")
     parser.add_argument("-r", "--rev", required=False, type=str, help="Path to reverse fastq file")
     parser.add_argument("-s", "--sample", required=True, type=str, help="Sample ID")
-    args = parser.parse_args(argv)
+    args = parser.parse_args()
     
     _FWD = args.fwd
     _REV = args.rev
@@ -37,9 +37,9 @@ def parse_args(argv=None):
     return _FWD, _REV, _SAMPLE
 
 
-def main(argv=None):
+def main():
 
-    _FWD, _REV, _SAMPLE = parse_args(argv)
+    _FWD, _REV, _SAMPLE = parse_args()
 
     fwd_handle = gzip.open(_FWD, "rt")
     fwd_reads = SeqIO.to_dict(SeqIO.parse(fwd_handle, "fastq"))
@@ -54,25 +54,35 @@ def main(argv=None):
         rev_reads = SeqIO.to_dict(SeqIO.parse(rev_handle, "fastq"))
         rev_handle.close()
     
-    remove_lst = []
+    remove_set = set()
 
     for read_id in fwd_reads.keys():
 
-        if "N" in str(fwd_reads[read_id].seq):
-            print(read_id)
-            remove_lst.append(read_id)
+        fwd_read_seq = str(fwd_reads[read_id].seq)
+        if len(fwd_read_seq) < 100:
+            remove_set.add(read_id)
             continue
-        elif paired_end and "N" in str(rev_reads[read_id].seq):
+        elif "N" in fwd_read_seq:
             print(read_id)
-            remove_lst.append(read_id)
+            remove_set.add(read_id)
             continue
 
-    [ fwd_reads.pop(read_id) for read_id in remove_lst ]
+        if paired_end:
+            rev_read_seq = str(rev_reads[read_id].seq)
+            if len(rev_read_seq) < 100:
+                print(read_id)
+                remove_set.add(read_id)
+                continue
+            elif "N" in rev_read_seq:
+                print(read_id)
+                remove_set.add(read_id)
+                continue
+
+    [ fwd_reads.pop(read_id) for read_id in remove_set ]
     if paired_end:
-        [ rev_reads.pop(read_id) for read_id in remove_lst ]
+        [ rev_reads.pop(read_id) for read_id in remove_set ]
 
     if paired_end:
-        
         fwd_handle = bgzf.BgzfWriter(f"./{_SAMPLE}_noambig_1.fastq.gz", "wb")
         rev_handle = bgzf.BgzfWriter(f"./{_SAMPLE}_noambig_2.fastq.gz", "wb")
         

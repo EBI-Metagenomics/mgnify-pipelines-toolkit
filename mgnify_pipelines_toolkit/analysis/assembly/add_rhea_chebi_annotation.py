8f843acc2d8cf8b8c3763a527c05b8d84c299352
Sofia Ochkalova <so.ochkalova@gmail.com>
2024-11-12 13:16:19 +0000

    remove option to download rhea2chebi mapping if not provided

--- mgnify_pipelines_toolkit/analysis/assembly/add_rhea_chebi_annotation.py
+++ mgnify_pipelines_toolkit/analysis/assembly/add_rhea_chebi_annotation.py
@@ -15,16 +15,13 @@
 # limitations under the License.
 
 import argparse
-import gzip
 import hashlib
 import logging
-import re
 import sys
 from pathlib import Path
 
 from Bio import SeqIO
 import pandas as pd
-import requests
 
 
 logging.basicConfig(
@@ -35,15 +32,6 @@ logging.basicConfig(
 
 
 def main(proteins: Path, output: Path, rhea2chebi: Path):
-    logging.info("Step 0/3: Check Rhea-CHEBI mapping file...")
-    if not rhea2chebi:
-        logging.info("Rhea-CHEBI mapping is not provided. Starting download...")
-        download_path = Path(".")
-        rhea2chebi = download_and_convert_to_tsv(download_path)
-        logging.info(
-            f"File downloaded successfully and saved to {rhea2chebi.resolve()}"
-        )
-
     logging.info(
         f"Step 1/3: Parse protein fasta and calculating SHA256 hash from {proteins.resolve()}"
     )
@@ -91,41 +79,6 @@ def main(proteins: Path, output: Path, rhea2chebi: Path):
     logging.info("Processed successfully. Exiting.")
 
 
-def download_and_convert_to_tsv(download_path: Path) -> Path:
-    url = "https://ftp.expasy.org/databases/rhea/txt/rhea-reactions.txt.gz"
-
-    # Ensure the download directory exists
-    download_path.mkdir(parents=True, exist_ok=True)
-
-    # Define paths for the created files
-    gz_file_path = download_path / "rhea-reactions.txt.gz"
-    tsv_file_path = download_path / "rhea2chebi.tsv"
-
-    # Download the .gz file
-    response = requests.get(url)
-    response.raise_for_status()
-    with gz_file_path.open("wb") as file:
-        file.write(response.content)
-
-    # Process the .gz file and convert it to TSV
-    with gzip.open(gz_file_path, "rt") as file_in, open(tsv_file_path, "w") as file_out:
-        input_text = file_in.read()
-        pattern = re.compile(
-            r"ENTRY\s+(.*?)\nDEFINITION\s+(.*?)\nEQUATION\s+(.*?)\n(?:ENZYME\s+(.*?))?\n*",
-            re.DOTALL,
-        )
-        matches = pattern.findall(input_text)
-        file_out.write("ENTRY\tDEFINITION\tEQUATION\tENZYME\n")
-        for match in matches:
-            entry, definition, equation, enzyme = match
-            enzyme = enzyme.strip() if enzyme else ""
-            file_out.write(f"{entry}\t{definition}\t{equation}\t{enzyme}\n")
-
-    # Clean up the downloaded .gz file
-    gz_file_path.unlink()
-    return tsv_file_path
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         """
@@ -151,7 +104,7 @@ if __name__ == "__main__":
         "--rhea2chebi",
         default=None,
         type=Path,
-        help="File that maps rhea_ids to CHEBI. Will be downloaded if not provided",
+        help="File that maps rhea_ids to CHEBI",
     )
 
     args = parser.parse_args()

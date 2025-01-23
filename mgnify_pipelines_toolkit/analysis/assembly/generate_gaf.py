0c20d1d2e45f756c5615d5d53915c5f30f185063
Christian Atallah <chrisata@ebi.ac.uk>
2025-01-23 14:35:59 +0000

    add license and shebang, make martin's changes to generate_gaf.py

--- mgnify_pipelines_toolkit/analysis/assembly/generate_gaf.py
+++ mgnify_pipelines_toolkit/analysis/assembly/generate_gaf.py
@@ -1,51 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2024 EMBL - European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import os
+import logging
+from typing import Set
 
 from mgnify_pipelines_toolkit.analysis.assembly.go_utils import parse_ips_file
 
-description = "Go slim pipeline."
-parser = argparse.ArgumentParser(description=description)
-parser.add_argument(
-    "-i", "--ips_input", help="InterProScan result file.", required=True
+# Constants
+PROJECT_NAME = "EBI Metagenomics"
+PROJECT_URL = "http://www.ebi.ac.uk/metagenomics"
+PROJECT_CONTACT = "metagenomics-help@ebi.ac.uk"
+FIXED_TIMESTAMP = "20160528"  # What is this timestamp?
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s: %(message)s"
 )
-parser.add_argument("-o", "--output", help="GO summary output file.", required=True)
-args = parser.parse_args()
-
-IPS_INPUT = args.ips_input
-OUTPUT = args.output
 
 
-def create_gaf_file(gaf_input_file_path, go_id_set):
+def write_gaf_file(gaf_input_file_path: str, go_id_set: Set[str]) -> None:
     """
-    :param gaf_input_file_path:
-    :param go2proteinDict:
-    :return: nothing
+    Create a GO Annotation File (GAF) from a set of GO IDs.
+
+    :param gaf_input_file_path: Path to output GAF file
+    :param go_id_set: Set of GO IDs to include in the file
     """
     with open(gaf_input_file_path, "w") as file:
+        # Write GAF header
         file.write("!gaf-version: 2.1\n")
-        file.write("!Project_name: EBI Metagenomics\n")
-        file.write("!URL: http://www.ebi.ac.uk/metagenomics\n")
-        file.write("!Contact Email: metagenomics-help@ebi.ac.uk\n")
+        file.write(f"!Project_name: {PROJECT_NAME}\n")
+        file.write(f"!URL: {PROJECT_URL}\n")
+        file.write(f"!Contact Email: {PROJECT_CONTACT}\n")
+
+        # Write GO entries
         for go_id in go_id_set:
-            gaf_file_entry_line_str = "EMG\t{0}\t{1}\t\t{2}\tPMID:12069591\tIEA\t\t{3}\t\t\tprotein\ttaxon:1310605\t{4}\t{5}\t\t".format(
-                go_id, "GO", go_id, "P", "20160528", "InterPro"
+            gaf_entry = "\t".join(
+                [
+                    "EMG",
+                    go_id,
+                    "GO",
+                    "",
+                    go_id,
+                    "PMID:12069591",
+                    "IEA",
+                    "",
+                    "P",
+                    "",
+                    "",
+                    "protein",
+                    "taxon:1310605",
+                    FIXED_TIMESTAMP,
+                    "InterPro",
+                    "",
+                ]
             )
-            file.write("" + gaf_file_entry_line_str + "\n")
+            file.write(gaf_entry + "\n")
+
+    logging.info(f"GAF file created successfully: {gaf_input_file_path}")
 
 
 def main():
+    """
+    Process the InterProScan TSV output and generate a GO annotation file (GAF)).
+    """
+    description = "Go slim pipeline for processing InterProScan results"
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "-i", "--ips_input", help="InterProScan result file", required=True
+    )
+    parser.add_argument("-o", "--output", help="GO summary output file", required=True)
+    args = parser.parse_args()
 
-    if not os.stat(IPS_INPUT).st_size == 0:
-        # Parse InterProScan result file; map protein accessions and GO terms
-        print("Parsing InterProScan input: " + IPS_INPUT)
-        go2protein_count_dict = parse_ips_file(IPS_INPUT)
-        print("Finished parsing.")
+    # Validate input file
+    if not os.path.exists(args.ips_input):
+        raise FileNotFoundError(f"Input file not found: {args.ips_input}")
 
-        gaf_output_path = f"{OUTPUT}_ips_annotations.gaf"
-        print("Creating GAF file: " + gaf_output_path)
-        go_id_set = go2protein_count_dict.keys()
-        create_gaf_file(gaf_output_path, go_id_set)
-        print("Finished GAF file generation.")
+    if os.path.getsize(args.ips_input) == 0:
+        logging.warning("Input file is empty. Skipping processing.")
+        return
+
+    # Parse InterProScan result file
+    logging.info(f"Parsing InterProScan input: {args.ips_input}")
+    go2protein_count_dict = parse_ips_file(args.ips_input)
+    logging.info("Finished parsing InterProScan file")
+
+    logging.info("Writing the GAF file")
+    write_gaf_file(
+        f"{args.output}_ips_annotations.gaf", set(go2protein_count_dict.keys())
+    )
 
 
 if __name__ == "__main__":

b23927a33bf4c73a6311b62c5072aa4cf02ee4b7
Christian Atallah <chrisata@ebi.ac.uk>
2025-11-11 21:34:38 +0000

    mild permute_primers refactor

--- mgnify_pipelines_toolkit/analysis/amplicon/permute_primers.py
+++ mgnify_pipelines_toolkit/analysis/amplicon/permute_primers.py
@@ -1,12 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2024-2025 EMBL - European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 from itertools import product
 from pathlib import Path
 
 from Bio import SeqIO
 
+AMBIGUOUS_BASES_DICT = {
+    "R": ["A", "G"],
+    "Y": ["C", "T"],
+    "S": ["G", "C"],
+    "W": ["A", "T"],
+    "K": ["G", "T"],
+    "M": ["A", "C"],
+    "B": ["C", "G", "T"],
+    "D": ["A", "G", "T"],
+    "H": ["A", "C", "T"],
+    "V": ["A", "C", "G"],
+    "N": ["A", "C", "T", "G"],
+}
+
 
 def parse_args():
-
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-i",
@@ -25,21 +54,8 @@ def parse_args():
     return input_path, prefix
 
 
-def permute_seq(seq):
-
-    ambiguous_bases_dict = {
-        "R": ["A", "G"],
-        "Y": ["C", "T"],
-        "S": ["G", "C"],
-        "W": ["A", "T"],
-        "K": ["G", "T"],
-        "M": ["A", "C"],
-        "B": ["C", "G", "T"],
-        "D": ["A", "G", "T"],
-        "H": ["A", "C", "T"],
-        "V": ["A", "C", "G"],
-        "N": ["A", "C", "T", "G"],
-    }
+def permute_seq(seq: str) -> list:
+    # Get all sequence permutations based on ambiguous bases
 
     seq_template = []
 
@@ -47,7 +63,7 @@ def permute_seq(seq):
         if base in ["A", "C", "T", "G"]:
             seq_template.append(base)
         else:
-            seq_template.append(ambiguous_bases_dict[base])
+            seq_template.append(AMBIGUOUS_BASES_DICT[base])
 
     seq_permutations = []
     for combo in product(*seq_template):
@@ -56,14 +72,14 @@ def permute_seq(seq):
     return seq_permutations
 
 
-def make_primer_permutations(primers_dict, prefix):
-
+def write_primer_permutations(primers_dict: dict, prefix: str) -> None:
     with open(f"{prefix}_permuted_primers.fasta", "w") as fw:
         for primer_name, seq in primers_dict.items():
-
             primer_seq = seq.seq
             fw.write(f">{primer_name}\n{primer_seq}\n")
 
+            # F_auto/R_auto are a consistent names for PIMENTO inferred primers
+            # For PIMENTO std primers, names always end with either F or R
             if primer_name == "F_auto" or primer_name[-1] == "F":
                 strand = "F"
             elif primer_name == "R_auto" or primer_name[-1] == "R":
@@ -77,10 +93,9 @@ def make_primer_permutations(primers_dict, prefix):
 
 
 def main():
-
     input_path, prefix = parse_args()
     primers_dict = SeqIO.to_dict(SeqIO.parse(Path(input_path), "fasta"))
-    make_primer_permutations(primers_dict, prefix)
+    write_primer_permutations(primers_dict, prefix)
 
 
 if __name__ == "__main__":

fd4fd79fe53e1a7967ce02d8dabeedd27139d96d
Germana Baldi <Ge94@users.noreply.github.com>
2026-01-06 17:08:07 +0000

    Maybe like this?

--- mgnify_pipelines_toolkit/analysis/amplicon/make_asv_count_table.py
+++ mgnify_pipelines_toolkit/analysis/amplicon/make_asv_count_table.py
@@ -253,7 +253,7 @@ def main():
 
     try:
         taxa_df = pd.read_csv(taxa, sep="\t", dtype=str)
-    except pandas.errors.EmptyDataError:
+    except pd.errors.EmptyDataError:
         logging.error("No mapseq annotation found")
         exit()
 

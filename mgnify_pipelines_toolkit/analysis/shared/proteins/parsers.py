929b4c5078803513fae674d008882512bf548d08
Sofia Ochkalova <61541445+ochkalova@users.noreply.github.com>
2026-04-23 09:37:01 +0100

    correct copyright statement
    Co-authored-by: Martín Beracochea <mbc@ebi.ac.uk>

--- mgnify_pipelines_toolkit/analysis/shared/proteins/parsers.py
+++ mgnify_pipelines_toolkit/analysis/shared/proteins/parsers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2026Give examples  EMBL - European Bioinformatics Institute
+# Copyright 2026 EMBL - European Bioinformatics Institute
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
